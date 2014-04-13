9c8db748e7f3075979e123a955e675e8c5602a42
Gk <gk@gkparishphilp.com>
2014-04-13 13:54:25 -0700

    - adding theme

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.app_domain = ENV['APP_DOMAIN'] || 'localhost:3005'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.app_domain = ENV['APP_DOMAIN'] || 'www.jugglebugsapp.com'
+
+
 end
