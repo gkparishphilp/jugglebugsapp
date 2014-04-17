3f0493cb9660bcd37a053209d02ef94c19ba4494
Gk <gk@gkparishphilp.com>
2014-04-17 16:45:19 -0700

    - point sitemaps

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -5,7 +5,7 @@ SitemapGenerator::Sitemap.public_path = 'tmp/'
 
 SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new
 SitemapGenerator::Sitemap.sitemaps_host = "http://#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com/"
-SitemapGenerator::Sitemap.sitemaps_path = 'com/sitemaps/'
+SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 
 SitemapGenerator::Sitemap.create do
 	add '/'
