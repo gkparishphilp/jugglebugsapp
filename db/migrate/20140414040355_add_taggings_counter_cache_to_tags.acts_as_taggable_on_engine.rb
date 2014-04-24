6bd4a2b33b60aeae9b2d186bb87b1ce24c0fed10
Gk <gk@gkparishphilp.com>
2014-04-24 09:25:12 -0700

    - cleanup old migr

--- db/migrate/20140414040355_add_taggings_counter_cache_to_tags.acts_as_taggable_on_engine.rb
+++
@@ -1,9 +0,0 @@
-# This migration comes from acts_as_taggable_on_engine (originally 3)
-class AddTaggingsCounterCacheToTags < ActiveRecord::Migration
-	
-	def change
-		add_column :tags, :taggings_count, :integer, default: 0
-	end
-
-
-end
